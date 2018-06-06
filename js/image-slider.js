da2d282aca1b6df4fcc8dcbba03fa7b19234ec02
kizzaW <kieran.wilsonba21@gmail.com>
2018-06-06 19:38:05 +0100

    try again

--- js/image-slider.js
+++ js/image-slider.js
@@ -1,14 +1,15 @@
 var slideIndex = 0;
-showSlides();
 
 function showSlides() {
     var i;
     var slides = document.getElementsByClassName("Slides");
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].classList.remove('show')
+        slides[i].classList.add('hide')
     }
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 5000);
-} // Change image every 5 seconds
+    slides[slideIndex - 1].classList.add('show')
+    slides[slideIndex - 1].classList.remove('hide')
+
+}
