aa033bc15ea24f98aebfc4b6e5ae59e0105189eb
kizzaW <kieran.wilsonba21@gmail.com>
2018-07-12 12:51:01 +0100

    popup retry alternate method

--- js/image-slider.js
+++ js/image-slider.js
@@ -21,5 +21,3 @@ function showSlides() {
     }
 } // Change image duration 
 setInterval(showSlides, 8000)
-
-//function popupbox()   var popup = document.getElementById("contentPopup");popup.classList.toggle("show");}
