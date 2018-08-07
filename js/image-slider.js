d92d7a4cc71f5f29a853c8ea46dc06af1f47c89e
kizzaW <kieran.wilsonba21@gmail.com>
2018-08-07 19:10:20 +0100

    attempt to specify Modal box to open

--- js/image-slider.js
+++ js/image-slider.js
@@ -23,11 +23,10 @@ function showSlides() {
 setInterval(showSlides, 8000)
 
 
-function open_modal(id) {
-    var e = document.getElementById('this.modal');
-    if (e.style.display == "none") {
-        e.style.display = "block";
-    } else {
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    if (e.style.display == 'block')
         e.style.display = 'none';
-    }
+    else
+        e.style.display = 'block';
 }
