c43b0dff8d49b9434e797565f48244aa6a1bbfda
kizzaW <kieran.wilsonba21@gmail.com>
2018-08-22 17:24:01 +0100

    removal of js open modal attempting new method

--- js/image-slider.js
+++ js/image-slider.js
@@ -21,13 +21,3 @@ function showSlides() {
     }
 } // Change image duration 
 setInterval(showSlides, 8000)
-
-
-function toggle_visibility(id) {
-    var e = document.getElementById(id);
-    if (e.style.display == 'block')
-        e.style.display = 'none';
-    else
-        e.style.display = 'block)';
-}
-}
