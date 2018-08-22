d2f2919f86f002b985628ee68c4a8da1f7330472
kizzaW <kieran.wilsonba21@gmail.com>
2018-08-22 20:22:00 +0100

    close btn 3

--- js/image-slider.js
+++ js/image-slider.js
@@ -22,11 +22,11 @@ function showSlides() {
 } // Change image duration 
 setInterval(showSlides, 8000)
 
-
-function toggle_visibility(id) {
-    var e = document.getElementById(id);
-    if (e.style.display == 'block')
-        e.style.display = 'none';
-    else
-        e.style.display = 'block';
+function fadeIn() {
+    var e = document.getElementById("modal");
+    if (e.style.display === "none") {
+        e.style.display = "block";
+    } else {
+        e.style.display = "none";
+    }
 }
