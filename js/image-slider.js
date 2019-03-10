8ede0ba0b38039f31eab6c74cf4a6bd2ca02d0bc
kizzaW <kieran.wilsonba21@gmail.com>
2019-03-10 16:36:11 +0000

    js change

--- js/image-slider.js
+++ js/image-slider.js
@@ -23,7 +23,7 @@ function showSlides() {
 setInterval(showSlides, 8000)
 
 
-function toggleOpen(id) {
+function toggle_visibility(id) {
     var popout = document.getElementById(id);
     if (popout.style.opacity == '1')
         popout.style.opacity = '0';
